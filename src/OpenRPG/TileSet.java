4010ce7236c15494f7f881d9c7d44a12385a60bc
Matthew Norton <menorton15@hotmail.com>
2019-05-31 15:17:43 -0500

    Added constructors to TileSet class.

--- src/OpenRPG/TileSet.java
+++ src/OpenRPG/TileSet.java
@@ -1,8 +1,26 @@
 package OpenRPG;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public abstract class TileSet implements Tile{
    Graphics tileSet;
 
+   public TileSet(){
+
+   }
+
+   public TileSet(Graphics img){
+      tileSet = img;
+   }
+
+   public TileSet(String fileName){
+      try {
+         tileSet = ImageIO.read(new File(fileName)).getGraphics();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
 }
