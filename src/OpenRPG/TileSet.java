f417d83a66a5e718d207699a31b4d4fdb3d92511
Matthew Norton <menorton15@hotmail.com>
2019-05-31 17:33:26 -0500

    Added getTile method to Tiled interface, changed TileSet to a normal class, and added constructors and required methods to the TileSet class.

--- src/OpenRPG/TileSet.java
+++ src/OpenRPG/TileSet.java
@@ -5,22 +5,62 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public abstract class TileSet implements Tile{
+public class TileSet implements Tiled{
    Graphics tileSet;
+   int tileWidth, tileHeight;
+
 
    public TileSet(){
 
    }
 
-   public TileSet(Graphics img){
+   public TileSet(Graphics img, int height, int width){
       tileSet = img;
+      tileHeight = height;
+      tileWidth = width;
    }
 
-   public TileSet(String fileName){
+   public TileSet(String fileName, int height, int width){
       try {
          tileSet = ImageIO.read(new File(fileName)).getGraphics();
+          tileHeight = height;
+          tileWidth = width;
       } catch (IOException e) {
          e.printStackTrace();
       }
    }
+
+    @Override
+    public int getTileHeight() {
+        return tileHeight;
+    }
+
+    @Override
+    public int getTileWidth() {
+        return tileWidth;
+    }
+
+    @Override
+    public void setTileHeight(int height) {
+
+    }
+
+    @Override
+    public void setTileWidth(int width) {
+
+    }
+
+    @Override
+    public Graphics getTile(int index) {
+       /*********************************************************************************
+        * The following code should be moved to the TileConverter class
+        * *******************************************************************************
+       int x, y;
+       int StartY = index / (tileSet.getClipBounds().width / tileWidth) * tileHeight;
+       int StartX = index % (tileSet.getClipBounds().width / tileWidth) * tileWidth;
+       */
+
+       //TODO: replace the x and y values with values that have been gleaned from the tile converter
+       return tileSet.create(0,0,tileWidth,tileHeight);
+    }
 }
--- src/OpenRPG/Tiled.java
+++ src/OpenRPG/Tiled.java
@@ -1,8 +1,11 @@
 package OpenRPG;
 
+import java.awt.*;
+
 public interface Tiled {
     int getTileHeight();
     int getTileWidth();
     void setTileHeight(int height);
     void setTileWidth(int width);
+    Graphics getTile(int index);
 }
