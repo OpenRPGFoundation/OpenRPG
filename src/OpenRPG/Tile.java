b1ae28e632f9380d428fc8dab0568e51c23ea1b9
Matthew Norton <menorton15@hotmail.com>
2019-05-31 15:53:43 -0500

    Renamed Tile.java to Tiled.java and changed method names.

--- src/OpenRPG/Tile.java
+++
@@ -1,8 +0,0 @@
-package OpenRPG;
-
-public interface Tile {
-    int getHeight();
-    int getWidth();
-    void setHeight(int height);
-    void setWidth(int width);
-}
---
+++ src/OpenRPG/Tileed.java
@@ -0,0 +1,8 @@
+package OpenRPG;
+
+public interface Tileed {
+    int getTileHeight();
+    int getTileWidth();
+    void setTileHeight(int height);
+    void setTileWidth(int width);
+}
