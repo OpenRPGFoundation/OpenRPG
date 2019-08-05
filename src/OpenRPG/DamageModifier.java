2b2cd4c81a2ae2c119fc1d5d03f8bb163fc96839
Matthew Norton <menorton15@hotmail.com>
2019-08-05 16:57:40 -0600

    Changed DamageModifier from interface to class and completed DefaultAttack

--- src/OpenRPG/DamageModifier.java
+++ src/OpenRPG/DamageModifier.java
@@ -1,5 +1,19 @@
 package OpenRPG;
 
-public interface DamageModifier {
-    int modify(int base);
+class DamageModifier {
+
+    private float modifierValue;
+
+    DamageModifier(){
+        modifierValue = 0;
+    }
+
+    DamageModifier(float modifier){
+        modifierValue = modifier;
+    }
+
+
+    int modify(int base){
+     return (int)((float)base * modifierValue);
+    }
 }
--- src/OpenRPG/DefaultAttack.java
+++ src/OpenRPG/DefaultAttack.java
@@ -1,16 +1,27 @@
 package OpenRPG;
 
+import java.lang.reflect.Modifier;
+
 public class DefaultAttack implements Attack, AttackProcessor, Animation{
 
     private int baseDamage;
     private DamageModifier modifier;
 
     DefaultAttack(){
+
         baseDamage = 1;
+        modifier = new DamageModifier();
     }
 
     DefaultAttack(int baseDamage){
+
         this.baseDamage = baseDamage;
+        modifier = new DamageModifier();
+    }
+
+    DefaultAttack(int baseDamage, DamageModifier modifier){
+        this.baseDamage = baseDamage;
+        this.modifier = modifier;
     }
 
     @Override
@@ -26,7 +37,7 @@ public class DefaultAttack implements Attack, AttackProcessor, Animation{
 
     @Override
     public int calculateDamage(IParameters attackerStats, IParameters defenderStats, DamageModifier modifier) {
-        int attack = attackerStats.getParameter("Attack") + baseDamage - defenderStats.getParameter("Attack");
+        int attack = attackerStats.getParameter("Attack") + baseDamage - defenderStats.getParameter("Defense");
         attack = modifier.modify(attack);
 
         if (attack < 0) {
