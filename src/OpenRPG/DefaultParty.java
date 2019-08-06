cd995b013334d317038bd5276c85b6c4e38f16de
Matthew Norton <menorton15@hotmail.com>
2019-08-06 00:14:30 -0600

    Added methods to IParty and DefaultParty.

--- src/OpenRPG/DefaultParty.java
+++ src/OpenRPG/DefaultParty.java
@@ -36,4 +36,32 @@ public class DefaultParty implements IParty {
     public void addPartyMember(IGameCharacter character) {
         characterList.add(character);
     }
+
+    @Override
+    public void removePartyMember(IGameCharacter character) {
+        characterList.remove(character);
+    }
+
+    @Override
+    public void removeMemberByIndex(int index) {
+        characterList.remove(index);
+    }
+
+    @Override
+    public void switchMembers(IGameCharacter characterOne, IGameCharacter characterTwo) {
+        int index1 = characterList.indexOf(characterOne);
+        int index2 = characterList.indexOf(characterTwo);
+
+        characterList.add(index1, characterTwo);
+        characterList.add(index2, characterOne);
+    }
+
+    @Override
+    public void switchMembers(int indexOne, int indexTwo) {
+        IGameCharacter characterOne = getCharacterByIndex(indexOne);
+        IGameCharacter characterTwo = getCharacterByIndex(indexTwo);
+
+        characterList.add(indexOne, characterTwo);
+        characterList.add(indexTwo, characterOne);
+    }
 }
--- src/OpenRPG/IParty.java
+++ src/OpenRPG/IParty.java
@@ -7,4 +7,8 @@ public interface IParty{
     IGameCharacter getCharacter(IGameCharacter character);
     IGameCharacter getCharacterByIndex(int index);
     void addPartyMember(IGameCharacter character);
+    void removePartyMember(IGameCharacter character);
+    void removeMemberByIndex(int index);
+    void switchMembers(IGameCharacter characterOne, IGameCharacter characterTwo);
+    void switchMembers(int indexOne, int indexTwo);
 }
